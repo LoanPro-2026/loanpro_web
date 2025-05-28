9be442f8dcce1efb2dc2433ad07014b698135d44
jakshat296 <j.akshat296@gmail.com>
2025-05-28 15:33:15 +0530

    Remove layout.tsx and Dashboard/page.tsx

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // add more if needed
+  ],
+  darkMode: 'class',
+  theme: { extend: {} },
+  plugins: [],
+}
