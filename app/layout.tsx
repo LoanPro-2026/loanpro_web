aa9bef1ce15ffa2db900b0e5053b2d8ef36376c8
jakshat296 <j.akshat296@gmail.com>
2025-05-28 17:23:11 +0530

    Resolving Errors of Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,4 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       </html>
     </ClerkProvider>
   );
-<<<<<<< HEAD
 } 
-=======
-} 
->>>>>>> bf4952c084c624fdc7fed85428769f806a8d323e
